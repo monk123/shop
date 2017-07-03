ce22500d6ad2c794e600e00e5cbded8dfe435b3f
Vadim Ayupov <ayub94.mogilev@gmail.com>
2017-07-03 18:43:14 +0300

    add

--- service/src/main/java/com/netcracker/validator/validate/ValidatorService.java
+++ service/src/main/java/com/netcracker/validator/validate/ValidatorService.java
@@ -1,6 +1,10 @@
 package com.netcracker.validator.validate;
 
 public interface ValidatorService {
-
+    /**
+     *
+     * @param name
+     * @return
+     */
     boolean valid(final String name);
 }
